20659a0e2bc622e71b3e65ceda998acd2c7f019a
Edeilton Oliveira <edeiltonso@gmail.com>
2023-10-22 17:03:37 -0300

    add paths das views

--- enquetes/urls.py
+++ enquetes/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'enquetes'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    # tudo aqui tem prefixo "enquetes/"
+    path('', views.listar, name='listar'),
+    path('<int:question_id>/', views.detalhar, name='detalhar'),
+    path('<int:question_id>/resultados/', views.resultados, name='resultados'),
+    path('<int:question_id>/votar/', views.votar, name='votar'),
 ]
--- enquetes/views.py
+++ enquetes/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
 
-def index(request):
+def listar(request):
     return HttpResponse("Aqui aparecerá a lista de enquetes")
+
+def detalhar(request, question_id):
+    return HttpResponse("Você está votando na pergunta %s." % question_id)
+
+def resultados(request, question_id):
+    response = "Você está visualizando os resultados da pergunta %s."
+    return HttpResponse(response % question_id)
+
+def votar(request, question_id):
+    return HttpResponse("Você votou na pergunta %s." % question_id)
