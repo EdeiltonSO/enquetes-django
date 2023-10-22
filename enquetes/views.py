aec57a172ee0c4d3746d9a1172a5aaa0c8c53a63
Edeilton Oliveira <edeiltonso@gmail.com>
2023-10-22 18:35:43 -0300

    add html e retorna conteúdo dinâmico pras views de listagem e detalhes

--- enquetes/views.py
+++ enquetes/views.py
@@ -1,10 +1,19 @@
 from django.http import HttpResponse
+from .models import Pergunta
+from django.shortcuts import render
+from django.http import Http404
 
 def listar(request):
-    return HttpResponse("Aqui aparecerá a lista de enquetes")
+    ultimas = Pergunta.objects.order_by('-data_pub')[:3]
+    context = {'perguntas_list': ultimas}
+    return render(request, 'enquetes/index.html', context)
 
 def detalhar(request, question_id):
-    return HttpResponse("Você está votando na pergunta %s." % question_id)
+    try:
+        p = Pergunta.objects.get(pk=question_id)
+    except Pergunta.DoesNotExist:
+        raise Http404("Essa pergunta não existe!")
+    return render(request, 'enquetes/detalhar.html', {'pergunta': p})
 
 def resultados(request, question_id):
     response = "Você está visualizando os resultados da pergunta %s."
